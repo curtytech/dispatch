0a958813169f7bff5b34abdac660faf5c665537a
Phelipe Curty <71573615+curtytech@users.noreply.github.com>
2026-01-19 14:49:06 -0300

    enterprise icon

--- app/Filament/Resources/UserResource.php
+++ app/Filament/Resources/UserResource.php
@@ -23,7 +23,7 @@ class UserResource extends Resource
 {
     protected static ?string $model = User::class;
 
-    protected static ?string $navigationIcon = 'heroicon-o-users';
+    protected static ?string $navigationIcon = 'heroicon-o-building-office-2';
 
     protected static ?string $modelLabel = 'Empresa';
 
